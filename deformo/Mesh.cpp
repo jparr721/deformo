77cd9f6e6054bf6681a4c505fb787a536949a814
Jarred Parr <jparr721@gmail.com>
2021-05-19 16:27:37 -0600

    Generalize vectorize function

--- deformo/Mesh.cpp
+++ deformo/Mesh.cpp
@@ -5,24 +5,12 @@
 
 Mesh::Mesh(const Eigen::MatrixXf& V, const Eigen::MatrixXi& F,
            const Eigen::MatrixXi& T) {
-  Vectorize(positions, V);
-  Vectorize(faces, F);
-  Vectorize(tetrahedrals, T);
+  Vectorize<Eigen::VectorXf>(positions, V);
+  Vectorize<Eigen::VectorXi>(faces, F);
+  Vectorize<Eigen::VectorXi>(tetrahedrals, T);
 
   colors.resize(positions.rows(), positions.cols());
   for (int i = 0; i < positions.size(); ++i) {
     colors(i) = i % 3 == 0 ? 1.f : 0.f;
   }
 };
-
-void Mesh::Vectorize(Eigen::RowMajorVectorXf& out, const Eigen::MatrixXf& in) {
-  out.resize(in.rows() * in.cols(), 1);
-  Eigen::MatrixXf in_t = in.transpose();
-  out = Eigen::Map<Eigen::VectorXf>(in_t.data(), in_t.rows() * in_t.cols());
-}
-
-void Mesh::Vectorize(Eigen::RowMajorVectorXi& out, const Eigen::MatrixXi& in) {
-  out.resize(in.rows() * in.cols(), 1);
-  Eigen::MatrixXi in_t = in.transpose();
-  out = Eigen::Map<Eigen::VectorXi>(in_t.data(), in_t.rows() * in_t.cols());
-}
--- deformo/Mesh.h
+++ deformo/Mesh.h
@@ -1,10 +1,11 @@
 #pragma once
 
-#include "EigenTypes.h"
 #include <Eigen/Dense>
-#include <vector>
 #include <memory>
 #include <string>
+#include <vector>
+
+#include "EigenTypes.h"
 
 class Mesh {
  public:
@@ -21,14 +22,20 @@ class Mesh {
   [[nodiscard]] int size() { return positions.size(); }
 
   [[nodiscard]] const float* colors_data() { return colors.data(); }
-  [[nodiscard]] int colors_size_bytes() { return colors.size() * sizeof(float); }
+  [[nodiscard]] int colors_size_bytes() {
+    return colors.size() * sizeof(float);
+  }
   [[nodiscard]] int colors_size() { return colors.size(); }
 
   [[nodiscard]] const int* faces_data() { return faces.data(); }
   [[nodiscard]] int faces_size_bytes() { return faces.size() * sizeof(int); }
   [[nodiscard]] int faces_size() { return faces.size(); }
 
-  private:
-  void Vectorize(Eigen::RowMajorVectorXf& out, const Eigen::MatrixXf& in);
-  void Vectorize(Eigen::RowMajorVectorXi& out, const Eigen::MatrixXi& in);
+ private:
+  template <typename Map, typename In, typename Out>
+  void Vectorize(Out& out, const In& in) {
+    out.resize(in.rows() * in.cols(), 1);
+    In in_t = in.transpose();
+    out = Eigen::Map<Map>(in_t.data(), in_t.rows() * in_t.cols());
+  }
 };
