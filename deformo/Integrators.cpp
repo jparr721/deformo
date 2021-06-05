ceafd09501f4cffd5af2451f255f66ce3dcd4bf3
Jarred Parr <jparr721@gmail.com>
2021-06-04 20:48:14 -0600

    Remove debug statements.

--- deformo/Integrators.cpp
+++ deformo/Integrators.cpp
@@ -10,15 +10,10 @@ void ExplicitCentralDifferenceMethod::Solve(Eigen::VectorXf& positions,
         forces - (((stiffness_ - a2 * mass_matrix_) * positions) -
                   ((a0 * mass_matrix_) * previous_position));
 
-    utils::GTestDebugPrint(effective_load);
-    std::cerr << "==" << std::endl;
     const Eigen::VectorXf next_displacement =
         effective_mass_matrix_.solve(effective_load);
 
-    utils::GTestDebugPrint(next_displacement);
-    std::cerr << "==" << std::endl;
     positions += next_displacement;
-    utils::GTestDebugPrint(positions);
     acceleration =
         a0 * (previous_position - (2 * current_positions) + positions);
     velocity = a1 * ((-1 * previous_position) + positions);
